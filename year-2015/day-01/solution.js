f98e6f43d9c001c14d567eb10136dc6c5cf8264d
Scott Donnelly <scott@donnel.ly>
2020-12-08 05:57:29 +0000

    2015 01

---
+++ year-2015/day-01/solution.js
@@ -0,0 +1,61 @@
+const d = require('debug')('solution');
+const _ = require('lodash');
+
+const util = require('../../utils');
+const imp = require('../../imp');
+const parsers = require('../../parse');
+
+// First example and expected answers for each part.
+// Ignored if empty strings.
+const ex1 = ``;
+const ex1expectedP1 = ``;
+const ex1expectedP2 = ``;
+
+// Seconf example and expected answers for each part.
+// Ignored if empty strings.
+const ex2 = ``;
+const ex2expectedP1 = ``;
+const ex2expectedP2 = ``;
+
+/**
+ * Input parser.
+ * @param {raw} raw unmodified input string from input-01.txt
+ * @param {line} raw split on newlines, empty items removed, items trimmed
+ * @param {comma} raw split on commas, empty items removed, items trimmed
+ * @param {space} raw split on spaces, empty lines removed, items trimmed
+ * @param {multi} raw, split on double newlines, empty items removed, split again on newlines, items trimmed
+ */
+function parse({ raw, line, comma, space, multi }) {
+    return raw.split('');
+}
+
+function part1(input) {
+    let floor = 0;
+    input.forEach(i => {
+        floor += (i === '(' ? 1 : -1);
+    })
+    return floor
+}
+
+function part2(input) {
+    let floor = 0;
+    let i = 0;
+    while(floor !== -1) {
+        floor += (input[i] === '(' ? 1 : -1);
+        i++;
+    }
+    return i;
+}
+
+module.exports = {
+    ex1,
+    ex2,
+    ex1expectedP1,
+    ex1expectedP2,
+    ex2expectedP1,
+    ex2expectedP2,
+    part1,
+    part2,
+    parse,
+};
+
