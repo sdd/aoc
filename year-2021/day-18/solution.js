aaaff0247e2ca85ca9b7748df93c68c4f1caf006
Scott Donnelly <scott@donnel.ly>
2021-12-18 09:47:23 +0000

    2021 day 18 nightmare JS cleanup

--- year-2021/day-18/solution.js
+++ year-2021/day-18/solution.js
@@ -49,33 +49,15 @@ const ex2expectedP2 = ``;
 }
 
 function part1(input) {
-
-
-    const reduced = input.reduce((acc, seq) => {
-        const res = sfReduce([acc, seq]);
-        // console.log(`REDUCE OUTER STEP:${JSON.stringify(res)}`);
-        return res;
-    });
-
-    console.log(`FINAL: ${  JSON.stringify(reduced)}`);
-
-    // const sum = reduced.reduce((acc, seq) => sfadd(acc, seq));
-
-    // const mags = reduced.map(seq => calcMag(seq));
-
+    const reduced = input.reduce((acc, seq) => sfReduce([acc, seq]));
     return magnitude(reduced);
-    // return _.sum(mags);
 }
 
 function part2(input) {
-
     let maxMag = Number.MIN_SAFE_INTEGER;
     for(const a of _.range(0, input.length)) {
-        console.log(`a=${  a}`);
         for(const b of _.range(0, input.length)) {
-            if (a === b) {
-                continue;
-            }
+            if (a === b) { continue; }
 
             const sumab = sfReduce([ input[a], input[b] ]);
             maxMag = Math.max(maxMag, magnitude(sumab));
@@ -86,137 +68,96 @@ function part2(input) {
 }
 
 function magnitude(item) {
-    if (!Array.isArray(item)) {
-        return item;
-    } else {
-        return (magnitude(item[0]) * 3) + (magnitude(item[1]) * 2);
-    }
+    return Array.isArray(item) 
+        ? (magnitude(item[0]) * 3) + (magnitude(item[1]) * 2)
+        : item;
 }
 
 function sfReduce(seq) {
     let count = 1;
     let orig = seq;
     let result;
-    while (true) {
-        result = sfReduceStep(orig)[1];
+
+    while (++count < 600) { // arbitrary limit to prevent infinite loop in case of a bug
+        result = explode(orig);
 
         if (JSON.stringify(result) === JSON.stringify(orig)) {
-            // d('no explosion, check for split');
-            result = splitOuter(orig);
-            if (JSON.stringify(result) === JSON.stringify(orig)) {
-                // d('no split either');
-            } else {
-                // d('split occurred');
-            }
-        } else {
-            // d('explosion occurred');
+            // no explosion, check for split
+            result = split(orig);
         }
 
-        // d('STEP %s RESULT: %o', count, JSON.stringify(result));
-
-        count++;
-        if (count > 400) {
-            d('RECURSE LIMIT REACHED');
-            return result;
-        }
-        if (JSON.stringify(result) === JSON.stringify(orig)) {
+        if (result === orig) {
+            // no explosion or split. reduce complete.
             return result;
         } else {
             orig = result;
         }
     }
+    d('LOOP LIMIT REACHED');
+    return result;
 }
 
-function sfReduceStep(seq, depth = 0) {
+function explode(seq) {
+    return explodeInner(seq, 0)[1];
+}
+
+function explodeInner(seq, depth) {
     if (!Array.isArray(seq)) {
-        // d('reached a scalar');
         return [null, seq, null];
     }
     
     const [left, right] = seq;
-    // d('checking depth %d items: left=%o, right=%o', depth, left, right);
     if (depth === 3) {
         if (Array.isArray(left)) {
             // need to explode left item
-            // d('need to explode the left item (%o)', left);
-
             if (!Array.isArray(right)) {
                 // simple case, just add to scalar
-                // d('simple explode of left (%o) where right is scalar', left);
                 return [left[0], [0, right + left[1]], null];
             } else {
-                // d('explode left into right: %o', left);
+                // explode left into overflow left and right item
                 return [left[0], [0, addFromLeft(right, left[1])], null];
             }
             
         } else if (Array.isArray(right)) {
-            // d('simple explode of right (%o) where left is scalar', right);
-            // need to explode right item only, add to scalar in left
+            // explode right into left and overflow right
             return [null, [left + right[0], 0], right[1]];
 
         } else {
-            // d('no explosion needed');
             // two scalars, no explosion required
             return [null, seq, null];
         }
     }
 
-
-    const [leftPassedLeft, newLeft, leftPassedRight] = sfReduceStep(left, depth + 1);
+    // check LHS for explosion
+    const [leftPassedLeft, newLeft, leftPassedRight] = explodeInner(left, depth + 1);
     let rightPassedLeft = null;
     let newRight = right;
     let rightPassedRight = null;
 
     // only one explosion per action. only try right reduce if left didn't explode
     if (leftPassedLeft === null && leftPassedRight === null && JSON.stringify(left) === JSON.stringify(newLeft)) {
-        // d('checking for RHS explosion');
-        ([rightPassedLeft, newRight, rightPassedRight] = sfReduceStep(right, depth + 1));
+        // checking for RHS explosion
+        ([rightPassedLeft, newRight, rightPassedRight] = explodeInner(right, depth + 1));
     }
-
-    // d('post-attempt-explodes: %o', {leftPassedLeft, newLeft, leftPassedRight, rightPassedLeft, newRight, rightPassedRight })
 
     if (leftPassedRight === null) {
         if (rightPassedLeft === null) {
-            if (JSON.stringify(left) === JSON.stringify(newLeft) && JSON.stringify(right) === JSON.stringify(newRight)) {
-                // simple case: nothing blew up.
-                // check for splits.
-
-                return [leftPassedLeft, [newLeft, newRight], rightPassedRight];
-            } else {
-                // explosion happened but no passing between left and right
-                // d('explosion but no intra passing: %o. outers: ', [leftPassedLeft, rightPassedRight]);
-                return [leftPassedLeft, [newLeft, newRight], rightPassedRight];
-            }
+            // nothing doing
+            return [leftPassedLeft, [newLeft, newRight], rightPassedRight];
         } else {
-            // left was ok but val passed from right to left
+            // val passed from right to left
             if (!Array.isArray(newLeft)) {
-                // easy case, just add
                 return [leftPassedLeft, [newLeft + rightPassedLeft, newRight], rightPassedRight];
             } else {
-                // d('need to pass %o from right to left (%o)', rightPassedLeft, newLeft);
                 return [leftPassedLeft, [addFromRight(newLeft, rightPassedLeft), newRight], rightPassedRight];
             }
         }
     } else {
-        if (rightPassedLeft === null) {
-            // right was ok but val passed from left to right
-            if (!Array.isArray(newRight)) {
-                return [leftPassedLeft, [newLeft, newRight + leftPassedRight], rightPassedRight];
-            } else {
-                // d('need to pass %o from left to right (%o)', leftPassedRight, newRight);
-                const result = [leftPassedLeft, [newLeft, addFromLeft(newRight, leftPassedRight)], rightPassedRight];
-                // d('result of pass was %o', result)
-                return result;
-            }
+        // val passed from left to right
+        if (!Array.isArray(newRight)) {
+            return [leftPassedLeft, [newLeft, newRight + leftPassedRight], rightPassedRight];
         } else {
-            // crazy, val passed from left to right and right to left? don't think this can actually happen
-            if (!Array.isArray(newRight) && !Array.isArray(newLeft)) {
-                d('unexpected but handlable left-right pass');
-                return [leftPassedLeft, [newLeft + leftPassedRight, newRight + leftPassedRight], rightPassedRight];
-            } else {
-                d('crazy left-right array-array pass');
-                return [leftPassedLeft, [addFromRight(newLeft, rightPassedLeft), addFromLeft(newRight, leftPassedRight)], rightPassedRight];
-            }
+            return [leftPassedLeft, [newLeft, addFromLeft(newRight, leftPassedRight)], rightPassedRight];
         }
     }
 }
@@ -239,48 +180,30 @@ function addFromRight(pair, val) {
     }
 }
 
-function splitOuter(orig) {
-    const [left, right] = orig;
-
-    const splitLeft = split(left);
-    if (splitLeft !== left) {
-        // d('left was split: %o', [splitLeft, right]);
-        return [splitLeft, right];
-    } else {
-        const splitRight = split(right);
-        if (splitRight !== right) {
-            // d('right was split: %o', [left, splitRight]);
-            return [left, splitRight];
-        } else {
-            // d('no split');
-            return orig;
-        }
-    }
-}
-
 function split(item) {
-    // console.log(`split ${  item  }:`);
     if (!Array.isArray(item)) {
+        // item is scalar
         if (item > 9) {
-            // console.log(`splitting ${  item}`);
+            // need to split it
             return [Math.floor(item / 2), Math.ceil(item / 2)];
         } else {
-            // console.log(`not splitting ${  item}`);
+            // nothing doing
             return item;
         }
     } else {
+        // item is a pair
         const [left, right] = item;
         const splitLeft = split(left);
         if (splitLeft !== left) {
-            // console.log(`${left  }was split into ${  splitLeft}`);
+            // there was a split in the left item
             return [splitLeft, right];
         } else {
             const splitRight = split(right);
             if (splitRight !== right) {
-                // console.log(`${right  }was split into ${  splitRight}`);
+                // there was a split in the right item
                 return [left, splitRight];
             } else {
-                // console.log(`not splitting ${  item}`);
+                // nothing doing
                 return item;
             }
         }
