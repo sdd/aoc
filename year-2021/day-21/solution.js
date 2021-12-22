98f55692bf7800b218d467d15464f3476388f3e2
Scott Donnelly <scott@donnel.ly>
2021-12-22 11:43:49 +0000

    2021 day 21 JS optimisation

--- year-2021/day-21/solution.js
+++ year-2021/day-21/solution.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-bitwise */
 /* eslint-disable no-loop-func */
 const d = require('debug')('solution');
 const _ = require('lodash');
@@ -51,41 +52,101 @@ function part1(input) {
     return (--die) * score[playerIndex];
 }
 
+function stateToKey(state) {
+    return state.turn 
+        + (state.p[0] << 1) 
+        + (state.p[1] << 6) 
+        + (state.s[0] << 11) 
+        + (state.s[1] << 16); 
+}
+
+function keyToState(key) {
+    return {
+        turn: key & 1,
+        p: [
+            (key & (31 << 1)) >> 1,
+            (key & (31 << 6)) >> 6,
+        ],
+        s: [
+            (key & (31 << 11)) >> 11,
+            (key & (31 << 16)) >> 16,
+        ]
+    }
+}
+
+const rollCounts = new Counter();
+for(let r1 = 1; r1 <= 3; r1++) {
+    for(let r2 = 1; r2 <= 3; r2++) {
+        for(let r3 = 1; r3 <= 3; r3++) {
+            rollCounts.inc(r1 + r2 + r3);
+        }
+    }
+}
+
 function part2(input) {
     const initialState = { p: [input[0], input[1]], s: [0, 0], turn: 0 };
     
     let stateCounts = new Counter();
-    stateCounts.inc(JSON.stringify(initialState));
+    stateCounts.inc(stateToKey(initialState));
 
     const winCounts = [0, 0];
 
+    let maxStateVal = stateToKey(initialState);
+    let maxStateLen = 1;
+
     while(stateCounts.size) {
         const nextStateCounts = new Counter();
-        for(let [state, count] of stateCounts.entries()) {
-            state = JSON.parse(state);
+        maxStateLen = Math.max(maxStateLen, stateCounts.size);
+        for(const [key, count] of stateCounts.entries()) {
 
-            for(let r1 = 1; r1 <= 3; r1++) {
-                for(let r2 = 1; r2 <= 3; r2++) {
-                    for(let r3 = 1; r3 <= 3; r3++) {
-                        const p = ((state.p[state.turn] + r1 + r2 + r3 - 1) % 10) + 1;
-                        const s = state.s[state.turn] + p;
+            const turn = key & 1;
+            const p0 = (key & (31 << 1)) >> 1;
+            const p1 = (key & (31 << 6)) >> 6;
+            const s0 = (key & (31 << 11)) >> 11;
+            const s1 = (key & (31 << 16)) >> 16;
 
-                        if (s >= 21) {
-                            winCounts[state.turn] += count;
-                        } else {
-                            const nextState = _.cloneDeep(state);
-                            nextState.p[state.turn] = p;
-                            nextState.s[state.turn] = s;
-                            nextState.turn = 1 - nextState.turn;
-                            
-                            nextStateCounts.add(JSON.stringify(nextState), count);
-                        }
+            for(const [roll, rollCount] of rollCounts.entries()) {
+                let np;
+                let ns;
+                if (turn === 0) {
+                    np = ((p0 + roll - 1) % 10) + 1;
+                    ns = s0 + np;
+
+                    if (ns >= 21) {
+                        winCounts[turn] += (count * rollCount);
+                    } else {
+                        const newKey = 1
+                            + (np << 1) 
+                            + (p1 << 6)
+                            + (ns << 11) 
+                            + (s1 << 16);
+                        maxStateVal = Math.max(maxStateVal, newKey);
+                        
+                        nextStateCounts.add(newKey, (count * rollCount));
+                    }
+
+                } else {
+                    np = ((p1 + roll - 1) % 10) + 1;
+                    ns = s1 + np;
+
+                    if (ns >= 21) {
+                        winCounts[turn] += (count * rollCount);
+                    } else {
+                        const newKey = 0
+                            + (p0 << 1) 
+                            + (np << 6)
+                            + (s0 << 11) 
+                            + (ns << 16);
+                        maxStateVal = Math.max(maxStateVal, newKey);
+                        
+                        nextStateCounts.add(newKey, (count * rollCount));
                     }
                 }
             }
         }
         stateCounts = nextStateCounts;
     }
+    d('maxStateval: %d, maxStateLen: %d', maxStateVal, maxStateLen);
     return Math.max(...winCounts);
 }
 
