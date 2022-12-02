6267f0bf32d5440841c40757be4b8cc75e5c1091
Scott Donnelly <scott@donnel.ly>
2022-12-02 07:10:01 +0000

    2022 day 2

---
+++ year-2022/day-02/solution.js
@@ -0,0 +1,70 @@
+const d = require('debug')('solution');
+const _ = require('lodash');
+
+const util = require('../../utils');
+const imp = require('../../imp');
+const parsers = require('../../parse');
+
+// First example and expected answers for each part.
+// Ignored if empty strings.
+const ex1 = `A Y
+B X
+C Z`;
+const ex1expectedP1 = 15;
+const ex1expectedP2 = 12;
+
+// Second example and expected answers for each part.
+// Ignored if empty strings.
+const ex2 = ``;
+const ex2expectedP1 = ``;
+const ex2expectedP2 = ``;
+
+/**
+ * Input parser.
+ * @param {Object} arg collection of pre-parsed helpers
+ * @param {string} arg.raw unmodified input string from input-01.txt
+ * @param {Array<string>} arg.lines raw split on newlines, trimmed, empty filtered
+ * @param {Array<string|Number>} arg.alphanums alphanumeric groups in lines[0]
+ * @param {Array<Number>} arg.nums numeric values in lines[0]
+ * @param {Array<string>} arg.comma split on commas, trimmed, empty filtered 
+ * @param {Array<string>} arg.space split on spaces, trimmed, empty filtered
+ * @param {Array<string>} arg.chars split lines[0] on every char
+ * @param {Array<Array<string>} arg.multi split on double newlines, empty filtered, split again on newlines, trimmed
+ * @param {Array<Array<string>} arg.grid 2D char grid
+ */
+ function parse({ raw, lines, alphanums, nums, comma, space, chars, multi, grid }) {
+    return lines;
+}
+
+const MAP_P1 = {
+    'A X': 4, 'A Y': 8, 'A Z': 3,
+    'B X': 1, 'B Y': 5, 'B Z': 9,
+    'C X': 7, 'C Y': 2, 'C Z': 6,
+}
+
+const MAP_P2 = {
+    'A X': 3, 'A Y': 4, 'A Z': 8,
+    'B X': 1, 'B Y': 5, 'B Z': 9,
+    'C X': 2, 'C Y': 6, 'C Z': 7,
+}
+
+function part1(input) {
+    return _.sum(input.map(l => MAP_P1[l]));
+}
+
+function part2(input) {
+    return _.sum(input.map(l => MAP_P2[l]));
+}
+
+module.exports = {
+    ex1,
+    ex2,
+    ex1expectedP1,
+    ex1expectedP2,
+    ex2expectedP1,
+    ex2expectedP2,
+    part1,
+    part2,
+    parse,
+};
+
